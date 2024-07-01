3034ae0d3f99edada736b5d0968eeb957f29b8cb
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-01 14:59:25 +0600

    connect with task model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,6 +9,7 @@ class Project extends Model
 {
     use HasFactory;
 
+    // Define the relationship with the Task model
     public function tasks() {
         return $this->hasMany(Task::class);
     }
