1fded4cd93ebcf83cde08efc084234f5ff4e6fc0
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-04 02:15:06 +0600

    slectinput initiated

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,4 +1,6 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import {
   PROJECT_STATUS_CLASS_MAP,
@@ -26,15 +28,49 @@ export default function Index({ auth, projects }) {
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                   <tr className="text-nowrap">
-                    <th className="px-3 py-2">ID</th>
-                    <th className="px-3 py-2">Image</th>
-                    <th className="px-3 py-2">Name</th>
-                    <th className="px-3 py-2">Status</th>
-                    <th className="px-3 py-2">Create Date</th>
-                    <th className="px-3 py-2">Due Date</th>
-                    <th className="px-3 py-2">Created By</th>
-                    <th className="px-3 py-2">Actions</th>
-                    <th className="px-3 py-2"></th>
+                    <th className="px-3 py-3">ID</th>
+                    <th className="px-3 py-3">Image</th>
+                    <th className="px-3 py-3">Name</th>
+                    <th className="px-3 py-3">Status</th>
+                    <th className="px-3 py-3">Create Date</th>
+                    <th className="px-3 py-3">Due Date</th>
+                    <th className="px-3 py-3">Created By</th>
+                    <th className="px-3 py-3">Actions</th>
+                    <th className="px-3 py-3"></th>
+                  </tr>
+                </thead>
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput
+                        className="w-full"
+                        placeholder="Project Name"
+                        onBlur={(e) =>
+                          searchFieldChanged("name", e.target.value)
+                        }
+                        onKeyPress={(e) => onKeyPress("name", e)}
+                      />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput
+                        className="w-full"
+                        onChange={(e) =>
+                          searchFieldChanged("status", e.target.value)
+                        }
+                      >
+                        <option value="">Select Status</option>
+                        <option value="pending">Pending</option>
+                        <option value="in_progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </SelectInput>
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -55,26 +91,30 @@ export default function Index({ auth, projects }) {
                       <td className="px-3 py-2">
                         <span
                           className={
-                            "px-2 py-1 rounded text-white" +
+                            "px-2 py-1 rounded text-white " +
                             PROJECT_STATUS_CLASS_MAP[project.status]
                           }
                         >
                           {PROJECT_STATUS_TEXT_MAP[project.status]}
                         </span>
                       </td>
-                      <td className="px-3 py-2">{project.created_at}</td>
-                      <td className="px-3 py-2">{project.due_date}</td>
+                      <td className="px-3 py-2 text-nowrap">
+                        {project.created_at}
+                      </td>
+                      <td className="px-3 py-2 text-nowrap">
+                        {project.due_date}
+                      </td>
                       <td className="px-3 py-2">{project.createdBy.name}</td>
                       <td className="px-3 py-2">
                         <Link
                           href={route("project.edit", project.id)}
-                          classID="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                          className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
                         >
                           Edit
                         </Link>
                         <Link
                           href={route("project.destroy", project.id)}
-                          classID="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                          className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
                         >
                           Delete
                         </Link>
