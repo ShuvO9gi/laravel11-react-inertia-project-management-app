a6d6af41c94ac82459f7beb37ad5dde3beebc426
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-04 00:37:37 +0600

    pagination addded

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -5,7 +5,18 @@ export default function Pagination({ links }) {
     <nav className="text-center mt-4">
       {links.map((link) => (
         // <Link>{link.label}</Link>
-        <Link dangerouslySetInnerHTML={{ __html: link.label }}></Link>
+        <Link
+          href={link.url || ""}
+          key={link.label}
+          className={
+            "inline-block py-2 px-3 rounded-lg text-gray-200 text-xs " +
+            (link.active ? "bg-gray-950 " : " ") +
+            (!link.url
+              ? "!text-gray-500 cursor-not-allowed "
+              : "hover:bg-gray-950")
+          }
+          dangerouslySetInnerHTML={{ __html: link.label }}
+        ></Link>
       ))}
     </nav>
   );
