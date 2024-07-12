c4031916944d3ec00929b2015daaa1a5549bec45
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-13 00:27:06 +0600

    project show updated

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -16,7 +16,24 @@ export default function Show({ auth, project }) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900 dark:text-gray-100">SHow</div>
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+              <div>
+                <img
+                  src={project.image_path}
+                  alt=""
+                  className="w-full h-64 object-cover"
+                />
+              </div>
+              <div className="grid gap-1 grid-cols-2">
+                <div>
+                  <div>
+                    <label className="font-bold text-lg">Projec ID</label>
+                    <p className="mt-1">{project.id}</p>
+                  </div>
+                </div>
+                <div></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
