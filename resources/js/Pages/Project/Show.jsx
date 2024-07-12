c20fda94153a008d82f668c281a64667261ab5df
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-13 00:43:12 +0600

    project show page updated

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -21,14 +21,14 @@ export default function Show({ auth, project }) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div>
+              <img
+                src={project.image_path}
+                alt=""
+                className="w-full h-64 object-cover"
+              />
+            </div>
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              <div>
-                <img
-                  src={project.image_path}
-                  alt=""
-                  className="w-full h-64 object-cover"
-                />
-              </div>
               <div className="grid gap-1 grid-cols-2 mt-2">
                 <div>
                   <div>
