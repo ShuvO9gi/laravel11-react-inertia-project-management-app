39ed4d1d137d6c148d7abc0a6ff3f5a7a24571d9
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-12 23:49:37 +0600

    project index updated

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -184,7 +184,11 @@ export default function Index({ auth, projects, queryParams = null }) {
                             alt=""
                           />
                         </td>
-                        <td className="px-3 py-2">{project.name}</td>
+                        <td className="px-3 py-2 text-gray-100 text-nowrap hover:underline">
+                          <Link href={route("project.show", project.id)}>
+                            {project.name}
+                          </Link>
+                        </td>
                         <td className="px-3 py-2 text-nowrap">
                           <span
                             className={
