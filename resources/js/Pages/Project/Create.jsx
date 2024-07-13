82bc5c9924d3136c339680a9715750245797336b
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-14 02:01:47 +0600

    project create updated

--- resources/js/Pages/Project/Create.jsx
+++ resources/js/Pages/Project/Create.jsx
@@ -1,7 +1,25 @@
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import TextAreaInput from "@/Components/TextAreaInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Description } from "@headlessui/react";
+import { Head, useForm } from "@inertiajs/react";
 
 export default function Create({ auth }) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    image: "",
+    name: "",
+    status: "",
+    description: "",
+    due_date: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    post(route("project.create"));
+  };
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -18,7 +36,53 @@ export default function Create({ auth }) {
       <div className="py-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900 dark:text-gray-100">Form</div>
+            <form
+              onSubmit={onSubmit}
+              className="p-4 sm:p-8 dark:bg-gray-800 shadow sm:rounded-lg"
+            >
+              <div>
+                <InputLabel
+                  htmlFor="project_image_path"
+                  value="Project Image"
+                />
+                <TextInput
+                  id="project_image_path"
+                  type="file"
+                  name="image"
+                  value={data.image}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData("image", e.target.value)}
+                />
+                <InputError message={errors.image} className="mt-2" />
+              </div>
+              <div className="mt-4">
+                <InputLabel htmlFor="project_name" value="Project Name" />
+                <TextInput
+                  id="project_name"
+                  type="text"
+                  name="name"
+                  value={data.name}
+                  className="mt-1 block w-full"
+                  isFocused={true}
+                  onChange={(e) => setData("name", e.target.value)}
+                />
+                <InputError message={errors.name} className="mt-2" />
+              </div>
+              <div className="mt-4">
+                <InputLabel
+                  htmlFor="project_description"
+                  value="Project Name"
+                />
+                <TextAreaInput
+                  id="project_description"
+                  name="description"
+                  value={data.description}
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData("description", e.target.value)}
+                />
+                <InputError message={errors.description} className="mt-2" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
