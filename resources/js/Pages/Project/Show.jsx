05b41192c1768b8ae64a6a67da7e83c67184d69f
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-14 01:05:07 +0600

    project show updated

--- resources/js/Pages/Project/Show.jsx
+++ resources/js/Pages/Project/Show.jsx
@@ -84,11 +84,15 @@ export default function Show({ auth, project, tasks, queryParams = null }) {
         </div>
       </div>
 
-      <div className="py-12">
+      <div className="pb-12">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
-              <TasksTable tasks={tasks} queryParams={queryParams} />
+              <TasksTable
+                tasks={tasks}
+                queryParams={queryParams}
+                hideProjectColumn={true}
+              />
             </div>
           </div>
         </div>
