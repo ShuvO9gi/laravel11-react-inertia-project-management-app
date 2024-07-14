2e683d02c597bfc69a5beab3102a7e1f5d1559d3
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-15 00:20:33 +0600

    delete functionality added

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -43,6 +43,14 @@ export default function Index({ auth, projects, success, queryParams = null }) {
     router.get(route("project.index", queryParams));
   };
 
+  const deleteProject = (project) => {
+    if (!window.confirm("Are you sure want to delete project?")) {
+      return;
+    }
+
+    router.delete(route("project.destroy", project.id));
+  };
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -219,19 +227,19 @@ export default function Index({ auth, projects, success, queryParams = null }) {
                           {project.due_date}
                         </td>
                         <td className="px-3 py-2">{project.createdBy.name}</td>
-                        <td className="px-3 py-2">
+                        <td className="px-3 py-2 text-nowrap">
                           <Link
                             href={route("project.edit", project.id)}
                             className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
                           >
                             Edit
                           </Link>
-                          <Link
-                            href={route("project.destroy", project.id)}
+                          <button
+                            onClick={(e) => deleteProject(project)}
                             className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
                           >
                             Delete
-                          </Link>
+                          </button>
                         </td>
                       </tr>
                     ))}
