acd5e90267aa831847dd340b7fe2c40ed9526902
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-14 15:33:25 +0600

    store validation for post create

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -23,6 +23,7 @@ class StoreProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
+            "image" => ["nullable", "jpg,jpeg,png"],
             "name" => ["required", "max:255"],
             "description" => ["string"],
             "due_date" => ["nullable", "date"],
