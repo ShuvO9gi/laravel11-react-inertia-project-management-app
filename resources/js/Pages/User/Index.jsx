fbce0ef27f18e967403e49989757150adcd6afc9
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-16 00:42:24 +0600

    user index updated

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -1,8 +1,6 @@
 import Pagination from "@/Components/Pagination";
-import SelectInput from "@/Components/SelectInput";
 import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { USER_STATUS_CLASS_MAP, USER_STATUS_TEXT_MAP } from "@/constants.jsx";
 import { Head, Link, router } from "@inertiajs/react";
 import TableHeading from "@/Components/TableHeading";
 
@@ -89,32 +87,6 @@ export default function Index({ auth, users, success, queryParams = null }) {
                       >
                         ID
                       </TableHeading>
-                      {/* <th onClick={(e) => sortChanged("id")}>
-                        <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
-                          ID
-                          <div>
-                            <ChevronUpIcon
-                              className={
-                                "w-4 " +
-                                (queryParams.sort_field === "id" &&
-                                queryParams.sort_direction === "asc"
-                                  ? "text-white"
-                                  : "")
-                              }
-                            />
-                            <ChevronDownIcon
-                              className={
-                                "w-4 -mt-2 " +
-                                (queryParams.sort_field === "id" &&
-                                queryParams.sort_direction === "desc"
-                                  ? "text-white"
-                                  : "")
-                              }
-                            />
-                          </div>
-                        </div>
-                      </th> */}
-                      <th className="px-3 py-3">Image</th>
                       <TableHeading
                         name={"name"}
                         sort_field={queryParams.sort_field}
@@ -124,12 +96,12 @@ export default function Index({ auth, users, success, queryParams = null }) {
                         Name
                       </TableHeading>
                       <TableHeading
-                        name={"status"}
+                        name={"email"}
                         sort_field={queryParams.sort_field}
                         sort_direction={queryParams.sort_direction}
                         sortChanged={sortChanged}
                       >
-                        Status
+                        Email
                       </TableHeading>
                       <TableHeading
                         name={"created_at"}
@@ -139,22 +111,11 @@ export default function Index({ auth, users, success, queryParams = null }) {
                       >
                         Created Date
                       </TableHeading>
-                      <TableHeading
-                        name={"due_date"}
-                        sort_field={queryParams.sort_field}
-                        sort_direction={queryParams.sort_direction}
-                        sortChanged={sortChanged}
-                      >
-                        Due Date
-                      </TableHeading>
-                      <th className="px-3 py-3">Created By</th>
                       <th className="px-3 py-3">Actions</th>
-                      <th className="px-3 py-3"></th>
                     </tr>
                   </thead>
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                     <tr className="text-nowrap">
-                      <th className="px-3 py-3"></th>
                       <th className="px-3 py-3"></th>
                       <th className="px-3 py-3">
                         <TextInput
@@ -168,22 +129,16 @@ export default function Index({ auth, users, success, queryParams = null }) {
                         />
                       </th>
                       <th className="px-3 py-3">
-                        <SelectInput
+                        <TextInput
                           className="w-full"
-                          defaultValue={queryParams["status"]}
-                          onChange={(e) =>
-                            searchFieldChanged("status", e.target.value)
+                          defaultValue={queryParams.name}
+                          placeholder="User Email"
+                          onBlur={(e) =>
+                            searchFieldChanged("email", e.target.value)
                           }
-                        >
-                          <option value="">Select Status</option>
-                          <option value="pending">Pending</option>
-                          <option value="in_progress">In Progress</option>
-                          <option value="completed">Completed</option>
-                        </SelectInput>
+                          onKeyPress={(e) => onKeyPress("email", e)}
+                        />
                       </th>
-                      <th className="px-3 py-3"></th>
-                      <th className="px-3 py-3"></th>
-                      <th className="px-3 py-3"></th>
                       <th className="px-3 py-3"></th>
                       <th className="px-3 py-3"></th>
                     </tr>
@@ -195,35 +150,13 @@ export default function Index({ auth, users, success, queryParams = null }) {
                         key={user.id}
                       >
                         <td className="px-3 py-2">{user.id}</td>
-                        <td className="px-3 py-2">
-                          <img
-                            src={user.image_path}
-                            style={{ width: 60 }}
-                            alt=""
-                          />
-                        </td>
                         <td className="px-3 py-2 text-gray-100 text-nowrap hover:underline">
-                          <Link href={route("user.show", user.id)}>
-                            {user.name}
-                          </Link>
+                          {user.name}
                         </td>
-                        <td className="px-3 py-2 text-nowrap">
-                          <span
-                            className={
-                              "px-2 py-1 rounded text-white " +
-                              USER_STATUS_CLASS_MAP[user.status]
-                            }
-                          >
-                            {USER_STATUS_TEXT_MAP[user.status]}
-                          </span>
-                        </td>
+                        <td className="px-3 py-2 text-nowrap">{user.email}</td>
                         <td className="px-3 py-2 text-nowrap">
                           {user.created_at}
                         </td>
-                        <td className="px-3 py-2 text-nowrap">
-                          {user.due_date}
-                        </td>
-                        <td className="px-3 py-2">{user.createdBy.name}</td>
                         <td className="px-3 py-2 text-nowrap">
                           <Link
                             href={route("user.edit", user.id)}
