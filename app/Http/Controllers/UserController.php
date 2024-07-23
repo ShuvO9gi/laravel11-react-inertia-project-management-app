9707c6816b0c5e8db0f010900f808bf3a19da6d7
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-23 21:29:41 +0600

    usercontroller updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,6 +50,7 @@ class UserController extends Controller
     public function store(StoreUserRequest $request)
     {
         $data = $request->validated();
+        $data["password"] = bcrypt($data["password"]);
 
         User::create($data);
 
