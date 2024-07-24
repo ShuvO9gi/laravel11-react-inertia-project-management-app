3a7ffa82cf0ab70c135838911e692b1453bfd86a
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-24 22:52:34 +0600

    validation data added

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -22,7 +23,9 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "name" => ["required", "string", "max:255"],
+            "email" => ["required", "string", "unique:users, email"],
+            "password" => ["required", "confirmed", Password::min(8)->letters()->mixedCase()->symbols()->numbers()]
         ];
     }
 }
