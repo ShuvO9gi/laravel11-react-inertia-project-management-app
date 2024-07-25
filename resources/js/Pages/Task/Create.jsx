796546573bdcf58e197f7f6131f0fff534ba8227
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-26 01:16:45 +0600

    create file updated

--- resources/js/Pages/Task/Create.jsx
+++ resources/js/Pages/Task/Create.jsx
@@ -106,6 +106,21 @@ export default function Create({ auth }) {
                 </SelectInput>
                 <InputError message={errors.status} className="mt-2" />
               </div>
+              <div className="mt-4">
+                <InputLabel htmlFor="task_priority" value="Task Priority" />
+                <SelectInput
+                  id="task_priority"
+                  name="priority"
+                  className="mt-1 block w-full"
+                  onChange={(e) => setData("priority", e.target.value)}
+                >
+                  <option value="">Select Priority</option>
+                  <option value="low">Low</option>
+                  <option value="medium">Medium</option>
+                  <option value="high">High</option>
+                </SelectInput>
+                <InputError message={errors.priority} className="mt-2" />
+              </div>
               <div className="mt-4 text-right">
                 <Link
                   href={route("task.index")}
