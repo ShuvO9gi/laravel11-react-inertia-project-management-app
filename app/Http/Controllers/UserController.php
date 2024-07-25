90272571204a26e2d046b96df0484917d8591bff
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-25 22:40:28 +0600

    email_verified_at added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,6 +51,7 @@ class UserController extends Controller
     public function store(StoreUserRequest $request)
     {
         $data = $request->validated();
+        $data["email_verified_at"] = time();
         $data["password"] = bcrypt($data["password"]);
 
         User::create($data);
