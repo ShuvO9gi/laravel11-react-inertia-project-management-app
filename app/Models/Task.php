e94598c7ff1808221063954c49fa40e8353df1e6
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-26 22:33:02 +0600

    some clarifiation added

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $fillable = [
+        "name","description","image_path","status","priority","due_date","assigned_user_id","created_by","updated_by","project_id"
+    ];
+
     use HasFactory;
     
     public function project() {
@@ -14,10 +18,12 @@ class Task extends Model
     }
     
     public function assignedUser() {
+        //'assigned_user_id' is the foreign key in the tasks table that references the id column in the users table.
         return $this->belongsTo(User::class, 'assigned_user_id');
     }
     
     public function createdBy() {
+        //'created_by' is the foreign key in the tasks table that references the id column in the users table.
         return $this->belongsTo(User::class, 'created_by');
     }
     public function updatedBy() {
