c77cd9edb1296f1a3096329706f26ed84abbe8d8
Md. Omar Faruq <shuvo.omar09@gmail.com>
2024-07-26 22:21:01 +0600

    users clarification added

--- database/migrations/2024_06_30_193722_create_tasks_table.php
+++ database/migrations/2024_06_30_193722_create_tasks_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('status');
             $table->string('priority');
             $table->string('due_date')->nullable();
-            $table->foreignId('assigned_user_id')->constrained('users');
+            $table->foreignId('assigned_user_id')->constrained('users'); // 'users' means users table
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('updated_by')->constrained('users');
             $table->foreignId('project_id')->constrained('projects');
